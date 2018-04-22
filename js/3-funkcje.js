b7280d4b8399b2b5c8eb642973e9622fbfd5bb57
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 11:23:37 +0200

    zmienne

--- js/3-funkcje.js
+++ js/3-funkcje.js
@@ -1,22 +1,35 @@
 'use strict'
 
-function aktualnyRok() {
-    console.log("2018");
+function dodajLiczby(pierwszaLiczba, drugaLiczba){
+    var wynik = pierwszaLiczba + drugaLiczba;
+//    console.log(wynik);
+    return wynik;
 }
-var aktualnyMiesiac = function(){
-    console.log("Kwiecień");
-}
 
-console.log(aktualnyMiesiac);
 
-aktualnyMiesiac();
-aktualnyMiesiac();
-aktualnyMiesiac();
-aktualnyMiesiac();
-aktualnyMiesiac();
-aktualnyMiesiac();
+dodajLiczby(4,7);
 
-aktualnyRok();
-aktualnyRok();
-aktualnyRok();
-aktualnyRok();
+var wynikZfunkcji = dodajLiczby(4,7);
+console.log(wynikZfunkcji);
+
+//function aktualnyRok() {
+//    console.log("2018");
+//}
+//var aktualnyMiesiac = function(){
+//    console.log("Kwiecień");
+//}
+//
+//console.log(aktualnyMiesiac);
+//
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//
+//aktualnyRok();
+//aktualnyRok();
+//aktualnyRok();
+//aktualnyRok();
+//
