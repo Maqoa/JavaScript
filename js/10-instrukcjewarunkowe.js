469bd3d90441842959a1903860d059ea5f0f3f33
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 14:48:54 +0200

    warunkowe

---
+++ js/10-instrukcjewarunkowe.js
@@ -0,0 +1,39 @@
+'use strict'
+
+
+
+var wzrostMateusz = 190;
+var wzrostOlgi = 190;
+
+//if (wzrostOlgi<wzrostMateusz){
+//    console.log("Olga jest niższa :)");
+//  }
+
+//if (wzrostOlgi>wzrostMateusz){
+//    console.log("Olga jest wyższa :)");
+//  }
+//else{console.log("Olga jest niższa :)");}
+
+//
+//if (wzrostOlgi > wzrostMateusz){
+//    console.log("Olga jest wyższa :)");
+//  }
+//else if (wzrostOlgi==wzrostMateusz){console.log("Olga jest  tak wysoka jak Mateusz :)");}
+//
+//else{console.log("Olga jest niższa :)");}
+
+var kolor = 'czarny';
+
+switch (kolor) {
+    case 'czerwony': console.log('Kolor czerwony')
+        break;
+         case 'zielony': console.log('Kolor zielony')
+        break;
+         case 'niebieski': console.log('Kolor niebieski')
+        break;
+    default:
+        console.log('Inny kolor')
+}
+
+
+
