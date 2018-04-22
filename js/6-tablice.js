21b2ee1e2cf28af69b17db31741667b72b00e915
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 12:26:31 +0200

    tablice

---
+++ js/6-tablice.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var imiona = ['Monika' , 'Krystian' , 'Łukasz'];
+
+imiona[3] = 'Tania';
+imiona[2] = 'Ela';
+
+imiona.push('Geralt');
+
+console.log(imiona);
+imiona.pop();
+
+imiona.pop();
+console.log(imiona);
+
+
+console.log(imiona.unshift('Robert'));
+console.log(imiona);
+
+console.log(imiona.shift());
+console.log(imiona);
+
+
+console.log(imiona.length);
+
+console.log(imiona.join(""));
+
+console.log(imiona.join(" "));
+
+console.log(imiona.join("  :)"));
+
+console.log(imiona.reverse());
+
+console.log(imiona.sort());
+
+var liczby = ["dwa", "trzy", "zero"];
+
+console.log(liczby.sort());
+
+
+console.log(liczby.sort().reverse());
+
+
+
+
+
+
+
+
+
+
