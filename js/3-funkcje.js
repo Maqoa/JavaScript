0c7bb2483447675613239abdcbb12486f14a20d9
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 10:49:24 +0200

    funkcje

--- js/3-funkcje.js
+++ js/3-funkcje.js
@@ -3,6 +3,18 @@
 function aktualnyRok() {
     console.log("2018");
 }
+var aktualnyMiesiac = function(){
+    console.log("Kwiecień");
+}
+
+console.log(aktualnyMiesiac);
+
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
 
 aktualnyRok();
 aktualnyRok();
