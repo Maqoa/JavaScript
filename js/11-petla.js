8c68b846f58cdd47994f767cd2ec8d165223cfd2
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 15:47:42 +0200

    pętla

---
+++ js/11-petla.js
@@ -0,0 +1,77 @@
+'use strict';
+
+//
+//for(var i=10; i>0; i--){console.log(i);}
+
+
+//for(var i=1; i<=5; i++){console.log(i);}
+//
+//
+
+//
+//var tablica= ["Krystian", "Monika", "Danuta"];
+//
+//for (var i=0; i<3; ++i) {console.log( "Element z indexem : " + i + " ma wartość " + tablica[i] );}
+//
+
+
+//var tablica= ["Krystian", "Monika", "Danuta"];
+//
+////for (var i=0; i<tablica.length; ++i) {console.log( "Element z indexem : " + i + " ma wartość " + tablica[i] );}
+//
+//tablica.forEach( function(element, index){console.log("Element z indexem : " + index + " ma wartość " + element);});
+
+
+//var it = 0;
+//while( it<10){
+//    console.log(it);
+//    it++
+//}
+//
+
+//
+//var iter = 8;
+//do {console.log(iter);
+//    iter++;
+//    console.log(iter);
+//    
+//} while( iter<10);
+//
+//
+
+var a = 0;
+while (a<10){
+    console.log(++a);
+    if (a == 5){
+        break;
+               }
+}
+
+
+
+
+
+
+for ( var b = 0; b<10 ; ++b){
+    if ( b==5){
+        break;
+    }else{console.log(b);}
+    console.log("--")
+}
+
+//
+//for ( var b = 0; b<10 ; ++b){
+//    if ( b==5){
+//        continue;
+//    }else{console.log(b);}
+//    console.log("--")
+//}
+//
+
+
+
+
+
+
+
+
