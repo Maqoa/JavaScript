f4f197b9080de17cf770abb0adba5137f62eab86
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 12:01:23 +0200

    typydanych

---
+++ js/5-typydanych.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var wyplata = 5000;
+var premia = 2400;
+var calkowitaWyplata;
+calkowitaWyplata = wyplata + premia;
+console.log(calkowitaWyplata);
+
+var wyplataStr = 5000;
+var premiaStr = 2400;
+var calkowitaWyplataStr;
+calkowitaWyplataStr = wyplataStr + premiaStr;
+console.log(calkowitaWyplataStr);
+
+var czyJestsmog;
+czyJestsmog = true;
+if (czyJestsmog){
+    console.log("Jest SMOG");
+}
+else{
+    console.log("Nie ma SMOGa ;");
+}
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
