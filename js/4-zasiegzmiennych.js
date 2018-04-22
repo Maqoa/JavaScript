db049039dbc5e6f3961cf0df89130d751122a451
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 11:43:37 +0200

    zmienne

---
+++ js/4-zasiegzmiennych.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var wiekSeweryna = 22;
+
+function multiply(l1, l2, l3){
+    
+   var result = l1*l2*l3;
+    console.log(wiekSeweryna);
+    return result;
+    
+    
+    
+}
+
+//var wynikMnozenia = multiply(3,4,5);
+
+//console.log(wynikMnozenia);
+
+console.log(wiekSeweryna);
+
+//var parametr4 = 10;
+//
+//function multiply( parametr1, parametr2, parametr3) {
+//    var result;
+//    var
+//}
