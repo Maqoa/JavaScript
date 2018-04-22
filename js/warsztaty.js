21fa70f62451ed13877d78cab796927f729ebac4
Maqoa <maciej.oleksiu@gmail.com>
2018-04-22 16:53:11 +0200

    coś z warsztatów

---
+++ js/warsztaty.js
@@ -0,0 +1,14 @@
+'use strict';
+var globalna = result;
+console.log(globalna);
+
+
+function multiply(l1, l2, l3){
+    
+   var result = l1*l2*l3;
+    console.log(wiekSeweryna);
+    return result;
+    
+    
+    
+}
